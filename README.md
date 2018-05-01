7bd84fedd2aed4aeac037e6826e320598d96246e
Ollie Phillips <oliver@eantics.co.uk>
2018-05-01 20:12:55 +0100

    send to github

---
+++ README.md
@@ -0,0 +1,3 @@
+## Sett is a golang package which offers a simple abstraction on Badger key/value store
+
+Work in progress. Based on badger v0.9.0 api
---
+++ sett.go
@@ -0,0 +1,114 @@
+package sett
+
+import (
+	"log"
+
+	"github.com/dgraph-io/badger"
+)
+
+var (
+	DefaultOptions         = badger.DefaultOptions
+	DefaultIteratorOptions = badger.DefaultIteratorOptions
+	BatchSize              = 100000
+)
+
+type Sett struct {
+	BatchSize int
+	db        *badger.DB
+	table     string
+}
+
+// Open is constructor function to create badger instance,
+// configure defaults and return struct instance
+func Open(opts badger.Options) *Sett {
+	s := Sett{}
+
+	// defaults
+	s.BatchSize = BatchSize
+
+	db, err := badger.Open(opts)
+	if err != nil {
+		log.Fatal("Open: create or open failed")
+	}
+	s.db = db
+	return &s
+}
+
+// Table selects the table, operations are to be performed
+// on. Used as a prefix on the keys passed to badger
+func (s *Sett) Table(table string) *Sett {
+	s.table = table
+	return s
+}
+
+// Set passes a key & value to badger. Expects string for both
+// key and value for convenience, unlike badger itself
+func (s *Sett) Set(key string, val string) error {
+	var err error
+	err = s.db.Update(func(txn *badger.Txn) error {
+		err = txn.Set([]byte(s.makeKey(key)), []byte(val))
+		return err
+	})
+	return err
+}
+
+// Get returns value of queried key from badger
+func (s *Sett) Get(key string) (string, error) {
+	var val []byte
+	var err error
+	err = s.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(s.makeKey(key)))
+		if err != nil {
+			return err
+		}
+		val, err = item.Value()
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return string(val), err
+}
+
+// SetAll creates/updates a map of key values in one go.
+// Dependent on size, multiple go routines will be used
+func (s *Sett) SetAll(batch map[string]string) error {
+	var err error
+	items := len(batch)
+	log.Println(items)
+	return err
+}
+
+// GetAll returns all values from a (virtual) table. An
+// optional filter allows the table prefix on the key search
+// to be expanded
+func (s *Sett) GetAll(filter ...string) (map[string]string, error) {
+
+}
+
+// Delete removes a key and its value from badger instance
+func (s *Sett) Delete(key string) error {
+	var err error
+	err = s.db.Update(func(txn *badger.Txn) error {
+		err = txn.Delete([]byte(s.makeKey(key)))
+		return err
+	})
+	return err
+}
+
+// Drop removes all keys with table prefix from badger,
+// the effect is as if a table was deleted
+func (s *Sett) Drop() error {
+	// get all keys with prefix and delete them
+}
+
+// Close wraps badger Close method for defer
+func (s *Sett) Close() error {
+	return s.db.Close()
+}
+
+func (s *Sett) makeKey(key string) string {
+	// makes the real key to be stored which
+	// comprises table name and key set
+	return s.table + key
+}
